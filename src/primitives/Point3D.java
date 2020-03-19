611bfce770c6b3d9dc621b252fe9b2134c38bce5
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-03-19 20:07:24 +0200

    first commit to branch

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,13 +1,20 @@
 package primitives;
 
+/**
+ * this class represent point in R3 dimension.
+ * the class hold 3 instance of coord to describe a point.
+ *
+ * @author yossef steinmetz & arie rosental
+ */
 public class Point3D {
-
 
     public Coordinate x;
     public Coordinate y;
     public Coordinate z;
-
-    Point3D ZERO = new Point3D(0,0,0);
+    /**
+     * instance of origin point
+     */
+   public static final Point3D ZERO = new Point3D(0,0,0);
     /**
      * ctor that gets three Coordinates
      * @param x
@@ -15,11 +22,17 @@ public class Point3D {
      * @param z
      */
     public Point3D(Coordinate x , Coordinate y , Coordinate z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+        this.x = new Coordinate(x);
+        this.y = new Coordinate(y);
+        this.z = new Coordinate(z);
     }
 
+    /**
+     * ctor that get three values params
+     * @param x
+     * @param y
+     * @param z
+     */
     public Point3D(double x , double y, double z){
         this.x = new Coordinate(x);
         this.y = new Coordinate(y);
@@ -36,14 +49,24 @@ public class Point3D {
         this.z = other.z;
     }
 
+    /**
+     * point3D getter
+     * @return coord value of plane x
+     */
     public double getX() {
         return x.get();
     }
-
+    /**
+     * point3D getter
+     * @return coord value of plane y
+     */
     public double getY() {
         return y.get();
     }
-
+    /**
+     * point3D getter
+     * @return coord value of plane Z
+     */
     public double getZ() {
         return z.get();
     }
@@ -66,5 +89,24 @@ public class Point3D {
     {
         double d = this.distanceSquared(other);
         return Math.sqrt(d);
+    }
+
+    @Override
+    public boolean equals (Object obj)
+    {
+        if (this== obj) return true;
+        if (obj==null) return false;
+        if (!(obj instanceof Point3D)) return false;
+        Point3D other = (Point3D) obj;
+        return x.equals(other.x) && y.equals(other.y) && z.equals(other.z);
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -2,22 +2,46 @@ package primitives;
 
 public class Vector {
     Point3D head;
-    Vector (Point3D in)
+    Vector (Point3D in) throws IllegalArgumentException
     {
-        this.head.x=in.x;
-        this.head.y=in.y;
-        this.head.z=in.z;
+        this.head.x = new Coordinate(in.x.get());
+        this.head.y = new Coordinate(in.y.get());
+        this.head.z = new Coordinate(in.z.get());
+        if (head.equals (Point3D.ZERO)) throw new IllegalArgumentException ("all arguments are zero");
     }
-    Vector (double x, double y, double z)
+    Vector(Coordinate x, Coordinate y, Coordinate z) throws IllegalArgumentException
     {
-        this.head.x= new Coordinate(x);
-        this.head.y= new Coordinate(y);
-        this.head.z= new Coordinate(z);
+        this.head.x = x;
+        this.head.y = y;
+        this.head.z = z;
+        if (head.equals (Point3D.ZERO)) throw new IllegalArgumentException ("all arguments are zero");
+    }
+    Vector (double x, double y, double z) throws IllegalArgumentException
+    {
+        this.head.x = new Coordinate(x);
+        this.head.y = new Coordinate(y);
+        this.head.z = new Coordinate(z);
+        if (head.equals (Point3D.ZERO)) throw new IllegalArgumentException ("all arguments are zero");
     }
     Vector (Vector other)
     {
-        this.head.x=other.head.x;
-        this.head.y= other.head.y;
-        this.head.z= other.head.z;
+        this.head.x = new Coordinate( other.head.getX());
+        this.head.y = new Coordinate( other.head.getY());
+        this.head.z = new Coordinate( other.head.getZ());
+    }
+    public boolean equals (Object obj)
+    {
+        if (this== obj) return true;
+        if (obj==null) return false;
+        if (!(obj instanceof Vector)) return false;
+        Vector other = (Vector) obj;
+        return head.equals(other);
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "head=" + head +
+                '}';
     }
 }
