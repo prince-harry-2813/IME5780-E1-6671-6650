be0a77b1c5ae10fa4abfbfad0c89ac13e31bdec6
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-03-19 23:27:32 +0200

    commit to branch

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -3,4 +3,10 @@ package primitives;
 public class Ray {
     Point3D p0;
     Vector dir;
+
+    public Ray(Point3D po, Vector vec) {
+        this.p0 = new Point3D(po);
+        this.dir = new Vector(vec);
+        this.dir.normalize();
+    }
 }
