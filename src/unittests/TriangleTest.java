094dd720f2051e3e7ba3e25c40a38c840ac88796
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-04-19 14:10:57 +0300

    V-1.2.1

--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -1,5 +1,16 @@
 package unittests;
 
-public class TriangleTest {
+import geometries.Triangle;
+import org.junit.Test;
+import primitives.Point3D;
+import primitives.Vector;
 
+import static org.junit.Assert.assertEquals;
+
+public class TriangleTest {
+    @Test
+    public void testGetNormal() {
+        Triangle tri = new Triangle(new Point3D(1, 0, 0), new Point3D(0, 1, 0), new Point3D(0, 0, 1));
+        assertEquals("wrong triangle normal", new Vector(Math.sqrt(1. / 3), Math.sqrt(1. / 3), Math.sqrt(1. / 3)), tri.getNormal(new Point3D(1, 0, 0)));
+    }
 }
