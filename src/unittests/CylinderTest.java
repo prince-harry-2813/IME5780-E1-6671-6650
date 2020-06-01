15ca162a4866a5a1634ad7399f388f27a7c8c918
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-06-01 23:17:57 +0300

    V-1.4.5 ______ Camera Test & integration java doc added bug fixed test added

--- src/unittests/CylinderTest.java
+++ src/unittests/CylinderTest.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 import primitives.Point3D;
 import primitives.Vector;
 
+import static org.junit.Assert.assertEquals;
+
 public class CylinderTest {
     @Test
     public void testGetNormal() {
         Cylinder cyl = new Cylinder(1., new Point3D(0, 0, 1), new Vector(0, 1, 0), 1.);
         Vector result = new Vector(cyl.getNormal(new Point3D(1, 2, 3)));
-        result.toString();
+        assertEquals("Wrong results", new Vector(0.3333333333333333, 0.6666666666666666, 0.6666666666666666), result);
         //to be continued
+
     }
 
 }
