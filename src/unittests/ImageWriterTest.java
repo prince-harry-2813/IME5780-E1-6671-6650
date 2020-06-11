78189cf8acabb8ac66181a7d0fea8ceba8aa2dc3
prince-harry-2813 <54502376+prince-harry-2813@users.noreply.github.com>
2020-06-11 14:04:59 +0300

    V-1.5.1 ______ added test ImageWriter.

--- src/unittests/ImageWriterTest.java
+++ src/unittests/ImageWriterTest.java
@@ -1,4 +1,27 @@
 package unittests;
 
+import org.junit.Test;
+import renderer.ImageWriter;
+
+import java.awt.*;
+
 public class ImageWriterTest {
+    @Test
+    /**
+     * crate image that grided by yellow stripes, the test run over the pixels in the file and write the color to every pixel.
+     */
+    public void imageWriterTest() {
+        ImageWriter gridPicture = new ImageWriter("gridPicture", 1000, 1600, 500, 800);
+        for (int i = 0; i < gridPicture.getNy(); i++) {
+            for (int j = 0; j < gridPicture.getNx(); j++) {
+                if (i % 50 == 0 || j % 50 == 0) {//if the correct pixel is divide by 50 paint it blue to crate a grid
+                    gridPicture.writePixel(j, i, Color.YELLOW);
+                } else gridPicture.writePixel(j, i, Color.CYAN); //the background of the rest picture is yellow
+            }
+        }
+        gridPicture.writeToImage();
+
+    }
+
+
 }
